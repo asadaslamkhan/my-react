fea65b77ecd56a2ccfe1198f1ec6ba28d7b261b8
Asad Aslam <your-email@example.com>
2026-04-13 16:23:03 +0500

    adding new pages

--- react-portfolio/src/components/layout/Navbar.jsx
+++ react-portfolio/src/components/layout/Navbar.jsx
@@ -2,34 +2,37 @@ import React, {useState, useEffect} from 'react'
 import {Code, Menu, X} from 'lucide-react'
 import {NAV_LINKS, PERSONAL_INFO} from '../../utils/constants'
 
-
 const Navbar = () => {
 
-  const [isMenuOpen, setIsMenuOpen] =useState(false);
-  const [isScrolled, setIsScrolled] =useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   const activeSection = useScrollSpy(NAV_LINKS.map(link => link.id));
 
 
   useEffect(() => {
-    const handleScroll = () => {
+    const handleScroll =() => {
       setIsScrolled(window.scrollY > 50);
     };
 
-      window.addEventListener('scroll', handleScroll);
-  return () => window.removeEventListener('scroll', handleScroll);
-},[]);
 
-  const handleNavClick = (sectionId) => {
-    scrollToSection(sectionId);
-    setIsMenuOpen(false);
-  };
+  window.addEventListener('scroll', handleScroll);
+  return() => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
- 
+
+    const handleNavClick = (sectionId) => {
+      scrollToSection(sectionId);
+      setIsMenuOpen(false);
+    };
+
+
 
 
   return (
-    <div>Navbar Content</div>
-  ) 
+    <div>
+      
+    </div>
+  )
 }
 
-export default Navbar; 
+export default Navbar
